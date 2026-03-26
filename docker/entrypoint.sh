c1ab0ae827f4073b890175e9918b52ecb096af2d
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2026-03-26 18:42:04 +0000

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+# Salir inmediatamente si un comando falla
 set -e
 
-if [[ -z "$TEST" ]]; then
+if [ -z "$TEST" ]; then
     export DOCS_REPO_URL="https://github.com/Yukics/docs"
     export DOCS_REPO_BRANCH="main"
     export DOCS_REPO_PATH="/workspaces/docs2"
@@ -13,12 +14,14 @@ if [[ -z "$TEST" ]]; then
     export QUARTZ_REPO_BUILD="/public"
 fi
 
-rm -rf $DOCS_REPO_PATH
-rm -rf $QUARTZ_REPO_PATH
+# Limpieza y Clones
+rm -rf "$DOCS_REPO_PATH"
+rm -rf "$QUARTZ_REPO_PATH"
 
-git clone --branch "$DOCS_REPO_BRANCH" "$DOCS_REPO_URL" $DOCS_REPO_PATH
-git clone --branch "$QUARTZ_REPO_BRANCH" "$QUARTZ_REPO_URL" $QUARTZ_REPO_PATH
+git clone --branch "$DOCS_REPO_BRANCH" "$DOCS_REPO_URL" "$DOCS_REPO_PATH"
+git clone --branch "$QUARTZ_REPO_BRANCH" "$QUARTZ_REPO_URL" "$QUARTZ_REPO_PATH"
 
-cd $QUARTZ_REPO_PATH
+# Instalación y Build
+cd "$QUARTZ_REPO_PATH"
 npm install
-npx quartz build -d $DOCS_REPO_PATH/obsidian -o $QUARTZ_REPO_BUILD
+npx quartz build -d "$DOCS_REPO_PATH/obsidian" -o "$QUARTZ_REPO_BUILD"
