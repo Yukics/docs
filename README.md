420692c8a90f8b172a25a5e87efa80c89ea3a5f4
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2026-03-26 19:27:22 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # docs
+
+This repo is meant to automate my obsidian vault via quartz
+
+The setup is meant to be deployed on kubernetes
+
+Feel free to copy and try
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
 
-# Salir inmediatamente si un comando falla
 set -e
 
-if [ -z "$TEST" ]; then
-    export DOCS_REPO_URL="https://github.com/Yukics/docs"
-    export DOCS_REPO_BRANCH="main"
-    export DOCS_REPO_PATH="/workspaces/docs2"
+# TESTING PURPOSES ONLY
+# export DOCS_REPO_URL="https://github.com/Yukics/docs"
+# export DOCS_REPO_BRANCH="main"
+# export DOCS_REPO_PATH="/workspaces/docs2"
+# export QUARTZ_REPO_URL="https://github.com/jackyzha0/quartz"
+# export QUARTZ_REPO_BRANCH="v4"
+# export QUARTZ_REPO_PATH="/workspaces/quartz"
+# export QUARTZ_REPO_BUILD="/public/html"
 
-    export QUARTZ_REPO_URL="https://github.com/jackyzha0/quartz"
-    export QUARTZ_REPO_BRANCH="v4"
-    export QUARTZ_REPO_PATH="/workspaces/quartz"
-    export QUARTZ_REPO_BUILD="/public"
-fi
-
-# Limpieza y Clones
 rm -rf "$DOCS_REPO_PATH"
 rm -rf "$QUARTZ_REPO_PATH"
 
 git clone --branch "$DOCS_REPO_BRANCH" "$DOCS_REPO_URL" "$DOCS_REPO_PATH"
 git clone --branch "$QUARTZ_REPO_BRANCH" "$QUARTZ_REPO_URL" "$QUARTZ_REPO_PATH"
 
-# Instalación y Build
 cd "$QUARTZ_REPO_PATH"
 rm -rf "${QUARTZ_REPO_BUILD:?}"/* 2>/dev/null || true
 npm install
-npx quartz build -d "$DOCS_REPO_PATH/obsidian" -o "$QUARTZ_REPO_BUILD"
+npx quartz build -d "$DOCS_REPO_PATH/obsidian" -o "/tmp$QUARTZ_REPO_BUILD"
+mv "/tmp$QUARTZ_REPO_BUILD" "$QUARTZ_REPO_BUILD"
