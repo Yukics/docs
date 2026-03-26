0dbedbca767ae80fee86973345398486ab4b745d
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2026-03-26 17:11:37 +0000

    init

---
+++ docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+export DOCS_REPO_URL="https://github.com/Yukics/docs"
+export DOCS_REPO_BRANCH="main"
+export DOCS_REPO_PATH="/workspaces/docs2"
+
+
+export QUARTZ_REPO_URL="https://github.com/jackyzha0/quartz"
+export QUARTZ_REPO_BRANCH="main"
+export QUARTZ_REPO_PATH="/workspaces/quartz"
+
+git clone --branch "$DOCS_REPO_BRANCH" "$DOCS_REPO_URL" $DOCS_REPO_PATH
+git clone --branch "$QUARTZ_REPO_BRANCH" "$QUARTZ_REPO_URL" $QUARTZ_REPO_PATH
+
+cd $QUARTZ_REPO_PATH
+rm -rf $QUARTZ_REPO_PATH/content
+npm install
+mv $DOCS_REPO_PATH/obsidian $QUARTZ_REPO_PATH/content
+npx quartz create
